b22e078d390a9bfb25688bc639ace219e7bd7d7a
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-12-04 16:04:44 -0500

    Update SceneDoor.gd

--- Scripts/FunctionalPlaceables/SceneDoor.gd
+++ Scripts/FunctionalPlaceables/SceneDoor.gd
@@ -4,4 +4,5 @@ export (String) var nextRoomPath
 
 func _on_SceneDoor_body_entered(body):
 	if body.name == "Player":
+		GlobalVariables.lastRoom = int(get_parent().get_parent().name)
 		get_tree().change_scene(nextRoomPath)
