6edad408e5a7259154806d9e19c5df95d20a9b8c
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-12-06 12:01:05 -0500

    Update Sibling.gd

--- Scripts/Characters/Birthplace/Sibling.gd
+++ Scripts/Characters/Birthplace/Sibling.gd
@@ -1,13 +1,15 @@
 extends StaticBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export (int) var abilityToGrant
+export (PoolStringArray) var dialouge
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _on_DialougeQue_body_entered(body):
+	if body.name == "Player":
+		runDialouge()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func runDialouge():
+	var lines = dialouge.size()
+	var line = 1
+	for line in dialouge:
+		get_parent().get_node("Player").printDialouge(line)
+	get_parent().get_node("Player").unlockAbility(abilityToGrant)
