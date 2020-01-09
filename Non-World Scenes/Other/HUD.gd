6d0399cae34826cef520228b0ad54cabd0f8fb4b
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2020-01-09 09:14:59 -0500

    Update HUD.gd

--- Non-World Scenes/Other/HUD.gd
+++ Non-World Scenes/Other/HUD.gd
@@ -6,17 +6,22 @@ var currentDialogLine = 0
 var dialog
 var speaker
 
-func _setDialouge(dialog_, speaker_):
+func _setDialog(dialog_, speaker_ = "???"):
 	dialog = dialog_
 	speaker = speaker_
 	runningDialog = true
 	dialogSize = dialog.size() - 1
 	if dialogSize >= currentDialogLine:
+		$DialogBox.show()
+		$DialogBox/Descale/Speaker.text = speaker
 		$DialogBox/Descale/Dialouge.text = dialog[currentDialogLine]
 	else:
+		runningDialog = false
 		$DialogBox.hide()
 		$DialogBox/Descale/Dialouge.text = " "
 		$DialogBox/Descale/Speaker.text = " "
 
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("ui_accept"):
+		currentDialogLine += 1
+		_setDialog(dialog, speaker)
